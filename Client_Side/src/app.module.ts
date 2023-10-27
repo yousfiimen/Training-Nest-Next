dd9e18759bf86d2c3a580d7a7059ea22856838f6
imen yousfi <imenyousfi44@gmail.com>
2023-10-27 15:49:04 +0100

    generate crud user

--- Client_Side/src/app.module.ts
+++ Client_Side/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env.development.local', '.env.development'],
+    isGlobal: true,
+
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
